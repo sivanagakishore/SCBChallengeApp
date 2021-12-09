a765f23b9820639fc523e0e2527fa205b12097af
sivanagakishore bitragunta <bsnkishore50@gmail.com>
2021-12-09 15:23:32 +0800

    scb challenge movies app

--- SCBMovieSample/SCBMovieSample/Features/MoviewDetails/MoviewDetailsView.swift
+++ SCBMovieSample/SCBMovieSample/Features/MoviewDetails/MoviewDetailsView.swift
@@ -82,7 +82,7 @@ struct MovieDetailView: View {
                             .frame(maxWidth: .infinity, alignment: .leading)
                     }
                 }
-            }
+            }.padding()
         }
     }
     
